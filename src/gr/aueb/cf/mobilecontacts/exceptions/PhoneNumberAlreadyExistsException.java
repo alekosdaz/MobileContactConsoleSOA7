01f4307b226866e6dcb03778e04057eddae99346
Alekos.Zour <alekosdaz@gmail.com>
2025-01-27 21:01:04 +0200

    Update

---
+++ src/gr/aueb/cf/mobilecontacts/exceptions/PhoneNumberAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.mobilecontacts.exceptions;
+
+public class PhoneNumberAlreadyExistsException extends Exception{
+
+    public PhoneNumberAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
